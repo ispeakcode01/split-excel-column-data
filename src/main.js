6b2168044a3e1a66737068d5e723b782eb83113c
Kateti Mareko <katetib16@gmail.com>
2020-04-19 01:37:44 +1200

    fix build error

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueWorker from 'vue-worker';
 
-Vue.use(VueWorker)
 Vue.config.productionTip = false
 new Vue({
   vuetify,
